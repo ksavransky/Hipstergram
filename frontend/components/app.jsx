bb6bcd5d26ad34a91429d4383a20f645ad1beb09
ksavransky <ksavransky@gmail.com>
2016-09-02 07:57:39 -0700

    working on navbar

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router';
-import GreetingContainer from './greeting/greeting_container';
+import NavBar from './navbar/navbar_container';
 import PostIndexContainer from './posts/post_index_container.jsx';
 
 const App = ({children}) => (
   <div>
     <header>
       <Link to="/" className="header-link"><h1>Hipstergram</h1></Link>
-      <GreetingContainer />
+      <NavBar />
     </header>
     <PostIndexContainer />
     {children}
---
+++ frontend/components/navbar/navbar.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const currentUser = this.props.currentUser;
+    const logout = this.props.logout;
+    return (
+      <div className="navbar">
+          <span className="navbar-logo">
+            Hip photo
+          </span>
+          <span className="navbar-text">
+            Hipstergram!
+          </span>
+          <span className="navbar-search-container">
+            Search here
+          </span>
+          <span className="navbar-photo-upload">
+            Photo upload here
+          </span>
+          <span className="navbar-user-photo">
+            User photo
+          </span>
+          <span className="navbar-user-name">
+            Hi, {currentUser.username}!
+          </span>
+          <span className="navbar-logout">
+            <button className="navbar-logout-button" onClick={logout}>Log Out</button>
+          </span>
+      </div>
+    );
+  }
+}
+
+// const sessionLinks = () => (
+//   <nav className="login-signup">
+//     <Link to="/login" activeClassName="current">Login</Link>
+//     &nbsp;or&nbsp;
+//     <Link to="/signup" activeClassName="current">Sign up!</Link>
+//   </nav>
+// );
+//
+// const personalGreeting = (currentUser, logout) => (
+// 	<hgroup className="header-group">
+// 		<h2 className="header-name">Hi, {currentUser.username}!</h2>
+// 		<button className="header-button" onClick={logout}>Log Out</button>
+// 	</hgroup>
+// );
+//
+// function Navbar({currentUser, logout}){
+//   if (currentUser){
+//     return personalGreeting(currentUser, logout);
+//   } else {
+//     return sessionLinks();
+//   }
+// }
+
+export default Navbar;
