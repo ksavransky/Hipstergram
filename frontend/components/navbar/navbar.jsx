d1eb49fbf5691752827078f58120a3759d322a8e
ksavransky <ksavransky@gmail.com>
2016-09-02 09:43:48 -0700

    logout button working

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
-import { Link } from 'react-router';
+import { Link, hashHistory, withRouter } from 'react-router';
 
 
 class Navbar extends React.Component {
   constructor(props) {
     super(props);
+    this.logout = this.props.logout;
+    this.redirectToLoggedIn = this.redirectToLoggedIn.bind(this);
+
+  }
+
+  redirectToLoggedIn(){
+    this.logout();
+  }
+
+  componentWillReceiveProps(newProps){
+    if (!newProps.currentUser){
+    this.props.router.replace("/login");
+    }
   }
 
   render() {
@@ -30,33 +43,11 @@ class Navbar extends React.Component {
           <span className="navbar-user-name">
             Hi, {currentUser.username}!
           </span>
-          <button className="navbar-logout-button" onClick={logout}>Log Out</button>
+          <button className="navbar-logout-button" onClick={this.redirectToLoggedIn}>Log Out</button>
       </div>
     );
   }
 }
 
-// const sessionLinks = () => (
-//   <nav className="login-signup">
-//     <Link to="/login" activeClassName="current">Login</Link>
-//     &nbsp;or&nbsp;
-//     <Link to="/signup" activeClassName="current">Sign up!</Link>
-//   </nav>
-// );
-//
-// const personalGreeting = (currentUser, logout) => (
-// 	<hgroup className="header-group">
-// 		<h2 className="header-name">Hi, {currentUser.username}!</h2>
-// 		<button className="header-button" onClick={logout}>Log Out</button>
-// 	</hgroup>
-// );
-//
-// function Navbar({currentUser, logout}){
-//   if (currentUser){
-//     return personalGreeting(currentUser, logout);
-//   } else {
-//     return sessionLinks();
-//   }
-// }
 
-export default Navbar;
+export default withRouter(Navbar);
