f6baaa1d13db10c596db9a2814164e745dbd5df9
ksavransky <ksavransky@gmail.com>
2016-09-08 13:51:03 -0700

    styling on following button done

--- frontend/components/users/user_show.jsx
+++ frontend/components/users/user_show.jsx
@@ -53,12 +53,12 @@ class UserShow extends React.Component {
 			if (following === false){
 				followButton = <div className="follow-button"
 					onClick={this.newFollow}>
-					follow
+					follow me
 				</div>;
 			} else {
-				followButton = <div className="follow-button"
+				followButton = <div className="unfollow-button"
 					onClick={this.unFollow}>
-					unfollow
+					stop following
 				</div>;
 			}
 		}
