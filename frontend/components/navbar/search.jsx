fc824a81e93f0270f0bc7229c820a9ff6e02dc73
ksavransky <ksavransky@gmail.com>
2017-05-17 11:21:07 -0700

    added up down controls to search

--- frontend/components/navbar/search.jsx
+++ frontend/components/navbar/search.jsx
@@ -8,10 +8,12 @@ class Search extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      inputVal: ''
+      inputVal: '',
+      selectedLi: null
     };
     this.selectName = this.selectName.bind(this);
     this.handleInput = this.handleInput.bind(this);
+    this.handleArrows = this.handleArrows.bind(this);
     this.names = [];
   }
 
@@ -25,6 +27,50 @@ class Search extends React.Component {
 
   handleInput(event) {
     this.setState({inputVal: event.currentTarget.value});
+  }
+
+  handleArrows(event) {
+
+    if(this.state.inputVal == ""){
+      this.setState({selectedLi: null});
+    }
+
+    let selectedLi = this.state.selectedLi;
+
+    function resetBold(){
+        $('.search-ul li').each((el) => {
+          $($('.search-ul li')[el]).css('font-weight', 'normal');
+        })
+    }
+    if(event.keyCode == 40 && $('.search-ul li').length > 0){
+      resetBold();
+      if(selectedLi == null || $('.search-ul li')[selectedLi + 1] == null){
+        selectedLi = 0;
+        this.setState({selectedLi: 0});
+      }
+      else {
+        selectedLi += 1;
+        this.setState({selectedLi: selectedLi});
+      }
+      $($('.search-ul li')[selectedLi]).css('font-weight', 'bold');
+    }
+    else if(event.keyCode == 38 && $('.search-ul li').length > 0){
+      resetBold();
+      if(selectedLi == null || selectedLi == 0){
+        selectedLi = $('.search-ul li').length - 1;
+        this.setState({selectedLi: selectedLi});
+      }
+      else {
+        selectedLi -= 1;
+        this.setState({selectedLi: selectedLi});
+      }
+      $($('.search-ul li')[selectedLi]).css('font-weight', 'bold');
+    }
+    else if(event.keyCode == 13){
+      $($('.search-ul li')[selectedLi]).trigger('click');
+      resetBold();
+      this.setState({selectedLi: null});
+    }
   }
 
   matches() {
@@ -72,6 +118,7 @@ class Search extends React.Component {
         <div className='search-box'>
           <input className="search-input"
             onChange={this.handleInput}
+            onKeyUp={this.handleArrows}
             value={this.state.inputVal}
             placeholder='Find Hipsters...'/>
           <ul className="search-ul">
